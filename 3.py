00afc54b72115f9cfe072436ec5b598ac64e0abf
Ashrith Sagar <ashrith9sagar@gmail.com>
2023-01-25 20:28:55 +0530

    c0.0.9 P3

--- 3.py
+++ 3.py
@@ -12,13 +12,21 @@ for test in range(TEST_CASES):
     tests.append((n, a, s))
 
 for n, a, s in tests:
-    print()
-    print(n, a, s)
+    # print()
+    # print(n, a, s)
+    FLAG = True
     splits = [char for char in s]
     uniq_spilts = set(splits)
-    checks = []
     for select_char in uniq_spilts:
+        checks = []
         for position, char in enumerate(s):
             if char == select_char:
+                # print(position)
                 checks.append(a[position])
-    print(checks)
+        if len(set(checks)) != 1:
+            FLAG = False
+            break
+    if FLAG:
+        print("YES")
+    else:
+        print("NO")
