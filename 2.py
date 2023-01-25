cbb08e306173df54f43d6781c6a765dcb2bb7555
Ashrith Sagar <ashrith9sagar@gmail.com>
2023-01-25 18:47:00 +0530

    c0.0.3 P2

---
+++ 2.py
@@ -0,0 +1,14 @@
+"""
+Multiverse Travellers
+"""
+
+m = int(input())
+
+demon = input()
+ex, fy = [int(x) for x in demon.split(" ")]
+
+travellers = input()
+cx, dy = [int(x) for x in travellers.split(" ")]
+
+portal = input()
+ax, by = [int(x) for x in portal.split(" ")]
