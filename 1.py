d1b55959f6e5d862f99016e4b80ef5a6f6ca4b81
Ashrith Sagar <ashrith9sagar@gmail.com>
2023-01-25 18:38:01 +0530

    c0.0.2 P1

--- 1.py
+++ 1.py
@@ -1,3 +1,20 @@
 """
 A Bitwise Assignment
 """
+
+TEST_CASES = int(input())
+
+tests = []
+for test in range(TEST_CASES):
+    m = int(input())
+    tests.append(m)
+
+for m in tests:
+    n = 0
+    while True:
+        n += 1
+        AND = m&n
+        XOR = m^n
+        if AND > 0 and XOR > 0:
+            break
+    print(n)
