20c011a9688c68c2a90ac4ff1a019b1c0770a156
Ashrith Sagar <ashrith9sagar@gmail.com>
2023-01-25 19:19:48 +0530

    c0.0.4 P2

--- 2.py
+++ 2.py
@@ -1,6 +1,8 @@
 """
 Multiverse Travellers
 """
+from itertools import product
+
 
 m = int(input())
 
@@ -12,3 +14,27 @@ cx, dy = [int(x) for x in travellers.split(" ")]
 
 portal = input()
 ax, by = [int(x) for x in portal.split(" ")]
+
+def in_bound(dim, delta):
+    return (dim+delta >= 1) and (dim+delta <= m)
+
+def calculate_radar(x, y):
+    radar = []
+    for delta in range(1, m+1):
+        radar.append((x, delta))
+        radar.append((delta, y))
+        if in_bound(x, delta) and in_bound(y, delta):
+            radar.append((x+delta, y+delta))
+        if in_bound(x, -1*delta) and in_bound(y, delta):
+            radar.append((x-delta, y+delta))
+        if in_bound(x, delta) and in_bound(y, -1*delta):
+            radar.append((x+delta, y-delta))
+        if in_bound(x, -1*delta) and in_bound(y, -1*delta):
+            radar.append((x-delta, y-delta))
+    radar = set(radar)
+    return radar
+
+demons_radar = calculate_radar(ex, fy)
+travellers_radar = calculate_radar(cx, dy)
+
+paths_to_portal = []
