8ad3c00884bf3e0ce0c9b371b0ddb54de7f1d4fd
Ashrith Sagar <ashrith9sagar@gmail.com>
2023-01-26 10:30:17 +0530

    c0.0.17 P2

--- Multiverse Travellers/main.py
+++ Multiverse Travellers/main.py
@@ -16,28 +16,25 @@ portal = input()
 ax, by = [int(x) for x in portal.split(" ")]
 
 def in_bound(dim, delta):
+    """ Check whether dimension will be within the board. Returns bool """
     return (dim+delta >= 1) and (dim+delta <= m)
 
-def calculate_radar(x, y):
+def calculate_radar(pos_x, pos_y):
+    """ Get all possible locations allowed. """
     radar = []
     for delta in range(1, m+1):
-        radar.append((x, delta))
-        radar.append((delta, y))
-        if in_bound(x, delta) and in_bound(y, delta):
-            radar.append((x+delta, y+delta))
-        if in_bound(x, -1*delta) and in_bound(y, delta):
-            radar.append((x-delta, y+delta))
-        if in_bound(x, delta) and in_bound(y, -1*delta):
-            radar.append((x+delta, y-delta))
-        if in_bound(x, -1*delta) and in_bound(y, -1*delta):
-            radar.append((x-delta, y-delta))
+        radar.append((pos_x, delta))
+        radar.append((delta, pos_y))
+        for mult in product([+1, -1], [+1, -1]):
+            if in_bound(pos_x, mult[0]*delta) and in_bound(pos_y, mult[1]*delta):
+                radar.append((pos_x+mult[0]*delta, pos_y+mult[1]*delta))
     radar = set(radar)
     return radar
 
 demons_radar = calculate_radar(ex, fy)
 travellers_radar = calculate_radar(cx, dy)
 
-reached = False
+REACHED = False
 blocks = []
 paths_to_portal = product(range(min(cx, ax), max(cx, ax)+1), range(min(dy, by), max(dy, by)+1))
 for path in paths_to_portal:
