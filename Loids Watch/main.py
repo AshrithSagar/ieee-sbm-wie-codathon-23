5f24d6328e59daf8bcdb90c529bde72952b7ee8a
Ashrith Sagar <ashrith9sagar@gmail.com>
2023-01-26 10:07:34 +0530

    c0.0.15 P4

--- Loids Watch/main.py
+++ Loids Watch/main.py
@@ -4,6 +4,7 @@ Loids Watch
 import math
 
 
+LARGE = 60 * 24  # Periodic with 1440 atleast.
 TEST_CASES = int(input())
 
 tests = []
@@ -13,31 +14,33 @@ for test in range(TEST_CASES):
     tests.append((s, x))
 
 def is_palindrome(num):
+    """ Check for palindrome. Returns bool. """
     return reverse(num) == num
 
 def reverse(num):
+    """ Return reverse of a number. Recursive function. """
     return int(num != 0) and ((num % 10) * (10**int(math.log(num, 10))) + reverse(num // 10))
 
+def append_0(num):
+    """ Add a 0 to the string if number is single digit. Returns string """
+    return ("0" if num < 10 else "") + str(num)
 
 for s, x in tests:
-    count = 0
+    COUNT = 0
     x = int(x)
-    palindromes = []
-    while True:
-        HH, MM = [int(d) for d in s.split(":")]
+    HH, MM = [int(d) for d in s.split(":")]
+    HH_0, MM_0 = HH, MM
+    for times in range(LARGE):
+        HH_s, MM_s = append_0(HH), append_0(MM)
+        print(HH_s, MM_s)
+        NUM = int("".join([HH_s, MM_s]))
+        print(NUM, is_palindrome(NUM))
+        COUNT += int(is_palindrome(NUM))
+
         MM += x
-
-        if MM > 59:
-            HH += 1
-            MM %= 60
-        if HH > 23:
-            HH %= 24
-
-        NUM = int("".join([str(HH), str(MM)]))
-        if NUM in palindromes:
+        HH += MM//60
+        HH %= 24
+        MM %= 60
+        if HH == HH_0 and MM == MM_0:
             break
-        delta = int(is_palindrome(NUM))
-        if delta:
-            palindromes.append(NUM)
-        count += delta
-    print(count)
+    print(COUNT)
